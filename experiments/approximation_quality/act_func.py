8bea340a74e161389ebdad8423b824b8fcd669ef
Moahmed Elsayed <mohamed.elsayed.1995@gmail.com>
2022-08-20 13:07:41 -0600

    reformat file

--- experiments/approximation_quality/act_func.py
+++ experiments/approximation_quality/act_func.py
@@ -1,7 +1,7 @@
 import torch
 
 activation_func = {
-    'tanh': torch.nn.Tanh,
-    'relu': torch.nn.ReLU,
-    'sigmoid': torch.nn.Sigmoid         
+    "tanh": torch.nn.Tanh,
+    "relu": torch.nn.ReLU,
+    "sigmoid": torch.nn.Sigmoid,
 }
