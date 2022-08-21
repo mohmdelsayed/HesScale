e6ebac81999dbe435a45d52b800fc647c49b3104
Moahmed Elsayed <mohamed.elsayed.1995@gmail.com>
2022-08-21 00:15:00 -0600

    refactor approximation quality experiment

--- experiments/ex_approx_quality.py
+++ experiments/ex_approx_quality.py
@@ -7,7 +7,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import yaml
-
+import pickle
 from utils import TABLEAU_COLORS, XKCD_COLORS
 from experiments.approximation_quality.experiment import HessQualityExperiment
 from experiments.approximation_quality.utils import unpack
@@ -33,51 +33,67 @@ normalizer = "HesScale"
 
 def main():
 
-    configs_dir = "experiments/approximation_quality"
-    data_lamda1 = {}
-    results_lamda1 = {}
+    dir_name = "data/ex_approx_quality"
+    configs_dir = "experiments/approximation_quality/configs"
+    file_name = "data_lambdas"
+    data_lambdas = {}
     with open(f"{configs_dir}/configs.yaml") as file:
         configs = yaml.full_load(file)
 
+    n_seeds = configs["n_seeds"]
     random.seed(54321)
-    randomlist = random.sample(range(0, 99999), configs["n_seeds"])
 
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+        randomlist = random.sample(range(0, 99999), n_seeds)
+        for lamda in lamda_range:
+            pool = Pool(configs["n_processes"])
+            results = pool.map(run, [(configs, seed, lamda) for seed in randomlist])
+            pool.close()
+            pool.join()
+            data_lambdas[lamda] = results
+
+        with open(f"{dir_name}/{file_name}.pkl", "wb") as f:
+            pickle.dump(data_lambdas, f)
+    else:
+        with open(f"{dir_name}/{file_name}.pkl", "rb") as f:
+            data_lambdas = pickle.load(f)
+
+    figure_1(lamda_range, data_lambdas, n_seeds, dir_name)
+    figure_2(data_lambdas[1], dir_name)
+    figure_3(data_lambdas[1], dir_name)
+
+
+def figure_1(lamda_range, data_lambdas, n_seeds, dir_name):
+    # Figure 1: plot the summed errors over the number of samples per each method
     for lamda in lamda_range:
-        data_lamdas = {}
-        pool = Pool(configs["n_processes"])
-        results = pool.map(run, [(configs, seed, lamda) for seed in randomlist])
-        pool.close()
-        pool.join()
-
-        # Figure 1: plot the summed errors over the number of samples per each method
-        for result in results:
+        data = {}
+        for result in data_lambdas[lamda]:
             for method in result:
                 sums = np.zeros_like(result[method][list(result[method].keys())[0]])
 
                 for name in result[method]:
                     sums += result[method][name]
 
-                if method in data_lamdas:
-                    data_lamdas[method].append(sums)
+                if method in data:
+                    data[method].append(sums)
                 else:
-                    data_lamdas[method] = [sums]
+                    data[method] = [sums]
 
         # per each method, compute bar means and std errors
         bar_means, bar_stds = [], []
-        methods = data_lamdas.keys()
+        methods = data.keys()
 
-        for method in data_lamdas:
-            bar_means.append(np.asarray(data_lamdas[method]).mean(axis=1).mean(axis=0))
-            bar_stds.append(np.asarray(data_lamdas[method]).mean(axis=1).std())
+        for method in data:
+            bar_means.append(np.asarray(data[method]).mean(axis=1).mean(axis=0))
+            bar_stds.append(np.asarray(data[method]).mean(axis=1).std())
 
         # save data for lambda=1 in a seperate variable, then plot bars for lambda=1
         if lamda == 1.0:
-            results_lamda1 = results.copy()
-            data_lamda1 = data_lamdas.copy()
             plt.bar(
                 methods,
                 bar_means,
-                yerr=bar_stds / (np.sqrt(configs["n_seeds"]) * 2),
+                yerr=bar_stds / (np.sqrt(n_seeds) * 2),
                 color="gray",
             )
 
@@ -92,16 +108,19 @@ def main():
         plt.ylabel("L1 Error")
 
     plt.legend(["λ = {}".format(lamda_i) for lamda_i in lamda_range])
-    plt.savefig("plot1.pdf")
+    plt.savefig(f"{dir_name}/plot1.pdf")
     plt.clf()
 
+
+def figure_2(data_lamda1, dir_name):
     # Figure 2: compute sum errors over the number of samples per each method per each layer
     errors_per_method_per_layer = {}
-    for i, result in enumerate(results_lamda1):
+    methods = data_lamda1[0].keys()
+    for i, result in enumerate(data_lamda1):
         for method in methods:
             if not method in errors_per_method_per_layer:
                 errors_per_method_per_layer[method] = np.zeros(
-                    (len(results_lamda1), len(result[method]) // 2)
+                    (len(data_lamda1), len(result[method]) // 2)
                 )
 
             erros_per_layer = []
@@ -129,14 +148,31 @@ def main():
     plt.ylabel("L1 Error")
     plt.xlabel("Layer Number")
     plt.ylim(bottom=0.0)
-    plt.savefig("plot2.pdf")
+    plt.savefig(f"{dir_name}/plot2.pdf")
     plt.clf()
+
+
+def figure_3(data_lambda1, dir_name):
 
     # Figure 3: Compute total L1 distance normalized by HesScale
     w = 0.8  # bar width
     colors = [XKCD_COLORS[key] for key in XKCD_COLORS]
+    data = {}
+    for result in data_lambda1:
+        for method in result:
+            sums = np.zeros_like(result[method][list(result[method].keys())[0]])
+
+            for name in result[method]:
+                sums += result[method][name]
+
+            if method in data:
+                data[method].append(sums)
+            else:
+                data[method] = [sums]
+
+    methods = data.keys()
     x = range(len(methods))  # x-coordinates of your bars
-    y = [np.asarray(data_lamda1[method]).mean(axis=1) for method in methods]
+    y = [np.asarray(data[method]).mean(axis=1) for method in methods]
     fig, ax = plt.subplots()
 
     normalized_y = y / y[list(methods).index(normalizer)]
@@ -169,7 +205,7 @@ def main():
     plt.xticks(x, methods)
     plt.axhline(y=1.0, color="r", alpha=0.2, linestyle="-", zorder=-12)
     plt.ylabel("Normalized L1 error")
-    plt.savefig("plot3.pdf")
+    plt.savefig(f"{dir_name}/plot3.pdf")
     plt.clf()
 
 
