372f0e07d2304c3724378bcd6eb1ef52cbbbedd7
Moahmed Elsayed <mohamed.elsayed.1995@gmail.com>
2022-09-22 23:44:17 -0600

    update the objective landscape experiment

--- experiments/exp3_landscape_trajectory.py
+++ experiments/exp3_landscape_trajectory.py
@@ -1,23 +1,14 @@
+# Adapted from https://github.com/jettify/pytorch-optimizer
 import torch
-from torch.optim import SGD, Adam
-from optim_landscape.artificial_landscapes import RastriginLoss, RosenbrockLoss
-import math
+from experiments.optim_landscape.artificial_landscapes import RosenbrockLoss
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from hyperopt import fmin, hp, tpe
 from backpack import backpack, extend
-from backpack.extensions import DiagHessian, DiagGGNExact, DiagGGNMC
 
 from hesscale import HesScale
-from experiments.computational_cost.optimizers.hesscale import HesScaleOptimizer
-from experiments.computational_cost.optimizers.ggn import GGNExactOptimizer
-from experiments.computational_cost.optimizers.ggn_mc import GGNMCOptimizer
-from experiments.computational_cost.optimizers.exact_diag_hess import (
-    ExactHessDiagOptimizer,
-)
-from experiments.computational_cost.optimizers.ada_hessian import Adahessian
-
+from experiments.computational_cost.optimizers.ada_hesscale import HesScaleOptimizer
 
 def execute_steps(func, initial_state, optimizer_class, optimizer_config, num_iter=500):
     x = torch.Tensor(initial_state).requires_grad_(True)
@@ -29,30 +20,10 @@ def execute_steps(func, initial_state, optimizer_class, optimizer_config, num_it
         optimizer.zero_grad()
         f = func(x)
         with backpack(HesScale()):
-            f.backward(create_graph=True)
+            f.backward()
         optimizer.step()
         steps[:, i] = x.detach().numpy()
     return steps
-
-
-def objective_rastrigin(n_steps=100):
-    def objective_rastrigin_(params):
-        lr = params["lr"]
-        optimizer_class = params["optimizer_class"]
-        initial_state = (-2.0, 3.5)
-        minimum = (0, 0)
-        optimizer_config = dict(lr=lr)
-        steps = execute_steps(
-            extend(RastriginLoss()),
-            initial_state,
-            optimizer_class,
-            optimizer_config,
-            n_steps,
-        )
-        return (steps[0][-1] - minimum[0]) ** 2 + (steps[1][-1] - minimum[1]) ** 2
-
-    return objective_rastrigin_
-
 
 def objective_rosenbrok(n_steps=100):
     def objective_rosenbrok_(params):
@@ -76,7 +47,34 @@ def objective_rosenbrok(n_steps=100):
 def execute_experiments(
     optimizers, objective, func, plot_func, initial_state, seed=1, n_steps=500
 ):
-    for item in optimizers:
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(1, 1, 1)
+
+    x = np.linspace(-3, 2, 250)
+    y = np.linspace(-2, 6, 250)
+    minimum = (1.0, 1.0)
+
+    X, Y = np.meshgrid(x, y)
+    Z = RosenbrockLoss(reduction="none")(torch.tensor(np.array([X, Y])))
+
+    ax.contour(X, Y, Z, 90, cmap="jet")
+
+    ax.set_title("Rosenbrock func")
+
+    TABLEAU_COLORS = {
+        "tab:blue": "#1f77b4",
+        "tab:orange": "#ff7f0e",
+        "tab:green": "#2ca02c",
+        "tab:red": "#d62728",
+        "tab:purple": "#9467bd",
+        "tab:brown": "#8c564b",
+        "tab:pink": "#e377c2",
+        "tab:gray": "#7f7f7f",
+        "tab:olive": "#bcbd22",
+        "tab:cyan": "#17becf",
+    }
+
+    for item, color in zip(optimizers, TABLEAU_COLORS):
         optimizer_class, lr_low, lr_hi = item
         space = {
             "optimizer_class": hp.choice("optimizer_class", [optimizer_class]),
@@ -89,7 +87,7 @@ def execute_experiments(
             max_evals=200,
             rstate=np.random.RandomState(seed),
         )
-        print(best["lr"], optimizer_class)
+
         steps = execute_steps(
             func,
             initial_state,
@@ -97,74 +95,24 @@ def execute_experiments(
             {"lr": best["lr"]},
             num_iter=n_steps,
         )
-        plot_func(steps, optimizer_class.__name__, best["lr"])
+
+        plot_func(ax, steps, optimizer_class.__name__, best["lr"], color, minimum)
+    ax.legend()
+    plt.savefig("rastrigin_{}.pdf".format(optimizer_class.__name__))
 
 
-def plot_rastrigin(grad_iter, optimizer_name, lr):
-    x = np.linspace(-4.5, 4.5, 250)
-    y = np.linspace(-4.5, 4.5, 250)
-    minimum = (0, 0)
-
-    X, Y = np.meshgrid(x, y)
-    Z = RastriginLoss(reduction="none")(torch.tensor(np.array([X, Y])))
+def plot_rosenbrok(ax, grad_iter, optimizer_name, lr, color, minimum):
     iter_x, iter_y = grad_iter[0, :], grad_iter[1, :]
-    fig = plt.figure(figsize=(8, 8))
-
-    ax = fig.add_subplot(1, 1, 1)
-    ax.contour(X, Y, Z, 20, cmap="jet")
-    ax.plot(iter_x, iter_y, color="r", marker="x")
-
-    ax.set_title(
-        "Rastrigin func: {} with "
-        "{} iterations, lr={:.6}".format(optimizer_name, len(iter_x), lr)
-    )
-    plt.plot(*minimum, "gD")
-    plt.plot(iter_x[-1], iter_y[-1], "rD")
-    plt.savefig("rastrigin_{}.png".format(optimizer_name))
-    # plt.show()
-
-
-def plot_rosenbrok(grad_iter, optimizer_name, lr):
-    x = np.linspace(-2, 2, 250)
-    y = np.linspace(-1, 3, 250)
-    minimum = (1.0, 1.0)
-
-    X, Y = np.meshgrid(x, y)
-    Z = RosenbrockLoss(reduction="none")(torch.tensor(np.array([X, Y])))
-
-    iter_x, iter_y = grad_iter[0, :], grad_iter[1, :]
-
-    fig = plt.figure(figsize=(8, 8))
-
-    ax = fig.add_subplot(1, 1, 1)
-    ax.contour(X, Y, Z, 90, cmap="jet")
-    ax.plot(iter_x, iter_y, color="r", marker="x")
-
-    ax.set_title(
-        "Rosenbrock func: {} with {} "
-        "iterations, lr={:.6}".format(optimizer_name, len(iter_x), lr)
-    )
-    plt.plot(*minimum, "gD")
-    plt.plot(iter_x[-1], iter_y[-1], "rD")
-    plt.savefig("rosenbrock_{}.png".format(optimizer_name))
-    # plt.show()
+    ax.plot(iter_x, iter_y, marker=".", color=color, label=optimizer_name+' ' +str(lr))
+    ax.plot(*minimum, "gD")
+    ax.plot(iter_x[-1], iter_y[-1], "D", color=color)
 
 
 optimizers = [
-    (torch.optim.Adam, -6, -0.1),
-    # (torch.optim.SGD, -6, -0.5),
-    (HesScaleOptimizer, -6.0, 0.0),
-    (Adahessian, -6, -0.0),
+    (torch.optim.Adam, -4.0, -0.1),
+    (torch.optim.SGD, -6.0, -0.1),
+    (HesScaleOptimizer, -4.0, -0.1),
 ]
-# execute_experiments(
-#     optimizers,
-#     objective_rastrigin,
-#     extend(RastriginLoss()),
-#     plot_func=plot_rastrigin,
-#     initial_state=(-2.5, 3.0),
-#     seed=678986,
-#     n_steps=500,
-# )
 
 execute_experiments(
     optimizers,
@@ -172,6 +120,6 @@ execute_experiments(
     extend(RosenbrockLoss()),
     plot_func=plot_rosenbrok,
     initial_state=(-2.0, 2.0),
-    seed=678986,
-    n_steps=100,
+    seed=12,
+    n_steps=499,
 )
