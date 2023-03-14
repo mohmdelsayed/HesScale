f147953f44c1970e8f4515c34bceb5b9e40cb9aa
Moahmed Elsayed <mohamed.elsayed.1995@gmail.com>
2023-03-14 16:21:35 -0600

    remove unncessary experiments

--- experiments/computational_cost/__init__.py
+++
@@ -1,9 +0,0 @@
-from torch.optim import SGD, Adam
-
-from experiments.approximation_quality.optimizers.ada_hessian import Adahessian
-from experiments.approximation_quality.optimizers.exact_diag_hess import (
-    ExactHessDiagOptimizer,
-)
-from experiments.approximation_quality.optimizers.ggn import GGNExactOptimizer
-from experiments.approximation_quality.optimizers.ggn_mc import GGNMCOptimizer
-from experiments.approximation_quality.optimizers.hesscale import HesScaleOptimizer
