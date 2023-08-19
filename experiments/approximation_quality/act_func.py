bb3535852502e93f4d6382fbe5580fbb2a0aee61
Moahmed Elsayed <mohamed.elsayed.1995@gmail.com>
2023-08-18 20:17:13 -0600

    remove experiments

--- experiments/approximation_quality/act_func.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-activation_func = {
-    "tanh": torch.nn.Tanh,
-    "relu": torch.nn.ReLU,
-    "sigmoid": torch.nn.Sigmoid,
-}
--- experiments/approximation_quality/utils.py
+++
@@ -1,9 +0,0 @@
-from functools import wraps
-
-
-def unpack(func):
-    @wraps(func)
-    def my_func(args):
-        return func(*args)
-
-    return my_func
