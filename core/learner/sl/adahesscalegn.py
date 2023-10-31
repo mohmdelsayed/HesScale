cff514b6c3489a2274df207d08e19fb8fc22ba98
Moahmed Elsayed <mohamed.elsayed.1995@gmail.com>
2023-10-31 10:45:20 -0600

    add variations of adahesscalegn

--- core/learner/sl/adahesscalegn.py
+++ core/learner/sl/adahesscalegn.py
@@ -1,8 +1,20 @@
 from core.learner.sl.learner import Learner
-from core.optim.adahesscalegn import AdaHesScaleGN
+from core.optim.adahesscalegn import AdaHesScaleGN, AdaHesScaleGNSqrt, AdaHesScaleGNAdamStyle
 
 class AdaHesScaleGNLearner(Learner):
     def __init__(self, network=None, optim_kwargs={}):
         optimizer = AdaHesScaleGN
         name = "adahesscalegn"
+        super().__init__(name, network, optimizer, optim_kwargs, extend=True)
+
+class AdaHesScaleGNSqrtLearner(Learner):
+    def __init__(self, network=None, optim_kwargs={}):
+        optimizer = AdaHesScaleGNSqrt
+        name = "adahesscalegn_sqrt"
+        super().__init__(name, network, optimizer, optim_kwargs, extend=True)
+
+class AdaHesScaleGNAdamStyleLearner(Learner):
+    def __init__(self, network=None, optim_kwargs={}):
+        optimizer = AdaHesScaleGNAdamStyle
+        name = "adahesscalegn_adamstyle"
         super().__init__(name, network, optimizer, optim_kwargs, extend=True)
