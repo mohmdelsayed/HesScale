f6abcd9a51544f48aac22e5706eb3c31ed83402f
Moahmed Elsayed <mohamed.elsayed.1995@gmail.com>
2023-10-31 15:23:57 -0600

    update experiment 3

--- experiments/ex3_label_permuted_mnist.py
+++ experiments/ex3_label_permuted_mnist.py
@@ -1,6 +1,7 @@
 from core.grid_search import GridSearch
 from core.learner.sl.sgd import SGDLearner
 from core.learner.sl.adam import AdamLearner
+from core.learner.sl.adahesscalegn import AdaHesScaleGNLearner, AdaHesScaleGNSqrtLearner, AdaHesScaleGNAdamStyleLearner
 from core.network.fcn_relu import FCNReLU
 from core.runner import Runner
 from core.run.sl_run import Run
@@ -25,11 +26,21 @@ adam_grid = GridSearch(
 
 
 
-grids = [sgd_grid] + [adam_grid]
+adahesscale_grid = GridSearch(
+                seed=[i for i in range(0, 20)],
+                lr=[10 ** -i for i in range(1, 5)],
+                network=[FCNReLU()],
+                n_samples=[1000000],
+    )
+
+grids = [sgd_grid] + [adam_grid] + [adahesscale_grid] + [adahesscale_grid for _ in range(3)]
 
 learners = [
     SGDLearner(),
     AdamLearner(),
+    AdaHesScaleGNLearner(),
+    AdaHesScaleGNSqrtLearner(),
+    AdaHesScaleGNAdamStyleLearner(),
 ]
 
 for learner, grid in zip(learners, grids):
