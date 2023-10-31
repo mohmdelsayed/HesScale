c7521ccfa2100f5f3bdd941b9af8cbd9a89545b5
Moahmed Elsayed <mohamed.elsayed.1995@gmail.com>
2023-10-30 19:38:14 -0600

    refactor plotter

--- core/plot/plotter.py
+++ core/plot/plotter.py
@@ -13,10 +13,11 @@ matplotlib.rcParams["ps.fonttype"] = 42
 matplotlib.rcParams.update({'font.size': 12})
 
 class Plotter:
-    def __init__(self, best_runs_path, task_name, avg_interval=1000):
+    def __init__(self, best_runs_path, task_name, avg_interval=10000, what_to_plot="losses"):
         self.best_runs_path = best_runs_path
         self.avg_interval = avg_interval
         self.task_name = task_name
+        self.what_to_plot = what_to_plot
 
     def plot(self):
         for subdir in self.best_runs_path:
@@ -25,7 +26,7 @@ class Plotter:
             for seed in seeds:
                 with open(f"{subdir}/{seed}") as json_file:
                     data = json.load(json_file)
-                    configuration_list.append(data["accuracies"])
+                    configuration_list.append(data[self.what_to_plot])
                     learner_name = data["learner"]
 
             configuration_list = np.array(configuration_list).reshape(len(seeds), len(configuration_list[0]) // self.avg_interval, self.avg_interval).mean(axis=-1)
@@ -33,17 +34,23 @@ class Plotter:
             std_list = np.array(configuration_list).std(axis=0) / np.sqrt(len(seeds))
             plt.plot(mean_list, label=learner_name)
             plt.fill_between(range(len(mean_list)), mean_list - std_list, mean_list + std_list, alpha=0.2)
-            plt.ylim([0.0, 1.0])
+            if self.what_to_plot == "losses":
+                plt.ylim([0.0, 2.5])
+                plt.ylabel("Online Loss")
+            else:
+                plt.ylim([0.0, 1.0])
+                plt.ylabel("Online Accuracy")
             plt.legend()
         
         plt.xlabel(f"Bin ({self.avg_interval} sample each)")
-        plt.ylabel("Accuracy")
-        plt.savefig("avg_losses.pdf", bbox_inches='tight')
+        plt.title(self.task_name)
+        plt.savefig("plot.pdf", bbox_inches='tight')
         plt.clf()
 
 
 if __name__ == "__main__":
-    best_runs = BestRun("ex5_stationary_mnist", "area", "fully_connected_relu", ["sgd"]).get_best_run(measure="accuracies")
+    what_to_plot = "accuracies"
+    best_runs = BestRun("exp1/stationary_mnist", "area", "fcn_relu", ["sgd"]).get_best_run(measure=what_to_plot)
     print(best_runs)
-    plotter = Plotter(best_runs, task_name="mnist")
+    plotter = Plotter(best_runs, task_name="MNIST", avg_interval=10000, what_to_plot=what_to_plot)
     plotter.plot()
