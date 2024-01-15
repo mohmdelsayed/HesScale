fix: ppo losses and hesscale derivatives

hesscale/core/additional_losses.py
@@ -114,7 +114,8 @@ class GaussianNLLLossMuPPO(_Loss):
             raise ValueError("Invalid reduction type")
     
     def get_prob(self, predicted_means: Tensor, predicted_vars: Tensor, actions:Tensor) -> Tensor:
-        dist = Normal(predicted_means, predicted_vars.maximum(torch.tensor(1e-8)))
+        action_std = torch.sqrt(predicted_vars)
+        dist = Normal(predicted_means, action_std.maximum(torch.tensor(1e-8)))
         return dist.log_prob(actions).sum(1).exp().unsqueeze(1)
     
 class GaussianNLLLossVarPPO(_Loss):
@@ -140,5 +141,6 @@ class GaussianNLLLossVarPPO(_Loss):
             raise ValueError("Invalid reduction type")
     
     def get_prob(self, predicted_means: Tensor, predicted_vars: Tensor, actions:Tensor) -> Tensor:
-        dist = Normal(predicted_means, predicted_vars.maximum(torch.tensor(1e-8)))
+        action_std = torch.sqrt(predicted_vars)
+        dist = Normal(predicted_means, action_std.maximum(torch.tensor(1e-8)))
         return dist.log_prob(actions).sum(1).exp().unsqueeze(1)

hesscale/core/derivatives_gn.py
@@ -284,7 +284,7 @@ class GaussianNLLLossMuPPODerivativesHesScale:
         ratio = probs / old_prob
         action = module.input2
         mu = module.input0
-        sigma = module.input1
+        sigma = torch.sqrt(module.input1)
         surr1 = ratio * adv
         surr2 = torch.clamp(ratio, 1.0 - module.epsilon, 1.0 + module.epsilon) * adv
 
@@ -308,7 +308,7 @@ class GaussianNLLLossVarPPODerivativesHesScale:
         ratio = probs / old_prob
         action = module.input2
         mu = module.input1
-        sigma = module.input0
+        sigma = torch.sqrt(module.input0)
         surr1 = ratio * adv
         surr2 = torch.clamp(ratio, 1.0 - module.epsilon, 1.0 + module.epsilon) * adv
 