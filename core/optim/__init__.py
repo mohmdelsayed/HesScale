a3713e331a6cfb6f05fa0eec20dc07fb15a77466
Homayoon Farrahi <farrahi@ualberta.ca>
2024-01-27 03:00:31 -0500

    new optimizers

--- core/optim/__init__.py
+++ core/optim/__init__.py
@@ -3,8 +3,11 @@ from .sgd_with_scaling import SGDScaled, SGDScaledSqrt
 from .adam import Adam
 from .adam_overshooting import AdamWithOvershootingPrevention
 from .adam_with_scaling import AdamScaled, AdamScaledSqrt
+from .adam_with_hesscale_scaling import AdamHesScale
 from .adahessian import AdaHessian
 from .adahessian_with_scaling import AdaHessianScaled
+from .adaggnmc import AdaGGNMC
+from .adaggnmc_with_scaling import AdaGGNMCScaled
 from .adahesscale import AdaHesScaleAdamStyle, AdaHesScaleSqrt, AdaHesScale
 from .adahesscale_with_scaling import AdaHesScaleAdamStyleScaled, AdaHesScaleSqrtScaled, AdaHesScaleScaled
 from .adahesscalegn import AdaHesScaleGNAdamStyle, AdaHesScaleGNSqrt, AdaHesScaleGN
